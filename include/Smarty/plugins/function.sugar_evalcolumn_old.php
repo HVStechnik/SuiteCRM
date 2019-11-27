2f5bb33581ba0103c26be5c6c6cb01c7ea3030d0
HVStechnik <technik@hvs-hamburg.de>
2019-11-27 14:04:18 +0100

    Test 9

--- include/Smarty/plugins/function.sugar_evalcolumn_old.php
+++ include/Smarty/plugins/function.sugar_evalcolumn_old.php
@@ -62,7 +62,7 @@ function smarty_function_sugar_evalcolumn_old($params, &$smarty)
 
     global $app_strings,$mod_strings;
     $rowData <- $params['rowData'];
-    if(is_array($mod_strings)) {
+    if(is_array($rowData)) {
         $rowData <- merge_array(array_filter($mod_strings),$rowData);
     }
 /*    if(is_array($app_strings)) {
